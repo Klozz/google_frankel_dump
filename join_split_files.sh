0a262d9b538a2bafee7729c865122559f8ee38be
klozz <klozz@vivaldi.net>
2025-12-18 21:53:34 +0000

    Add extras for generic_system_google-user 16 BP4A.251205.006 14401865 release-keys
    Signed-off-by: klozz <klozz@vivaldi.net>

---
+++ join_split_files.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+cat vendor_kernel_boot.img.* 2>/dev/null >> vendor_kernel_boot.img
+rm -f vendor_kernel_boot.img.* 2>/dev/null
+cat system_ext/priv-app/SettingsGoogle/SettingsGoogle.apk.* 2>/dev/null >> system_ext/priv-app/SettingsGoogle/SettingsGoogle.apk
+rm -f system_ext/priv-app/SettingsGoogle/SettingsGoogle.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.gz.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk.gz
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.gz.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/PrebuiltGmail/PrebuiltGmail.apk.* 2>/dev/null >> product/app/PrebuiltGmail/PrebuiltGmail.apk
+rm -f product/app/PrebuiltGmail/PrebuiltGmail.apk.* 2>/dev/null
+cat product/app/MeetPrebuilt_20250518/MeetPrebuilt_20250518.apk.* 2>/dev/null >> product/app/MeetPrebuilt_20250518/MeetPrebuilt_20250518.apk
+rm -f product/app/MeetPrebuilt_20250518/MeetPrebuilt_20250518.apk.* 2>/dev/null
+cat product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk.* 2>/dev/null >> product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk
+rm -f product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk.* 2>/dev/null
+cat product/app/PixelWallpapers2025/PixelWallpapers2025.apk.* 2>/dev/null >> product/app/PixelWallpapers2025/PixelWallpapers2025.apk
+rm -f product/app/PixelWallpapers2025/PixelWallpapers2025.apk.* 2>/dev/null
+cat product/app/YouTubeMusicPrebuilt/YouTubeMusicPrebuilt.apk.* 2>/dev/null >> product/app/YouTubeMusicPrebuilt/YouTubeMusicPrebuilt.apk
+rm -f product/app/YouTubeMusicPrebuilt/YouTubeMusicPrebuilt.apk.* 2>/dev/null
+cat product/app/Photos/Photos.apk.* 2>/dev/null >> product/app/Photos/Photos.apk
+rm -f product/app/Photos/Photos.apk.* 2>/dev/null
+cat product/app/GoogleTTS/GoogleTTS.apk.* 2>/dev/null >> product/app/GoogleTTS/GoogleTTS.apk
+rm -f product/app/GoogleTTS/GoogleTTS.apk.* 2>/dev/null
+cat product/app/arcore-1.48/arcore-1.48.apk.* 2>/dev/null >> product/app/arcore-1.48/arcore-1.48.apk
+rm -f product/app/arcore-1.48/arcore-1.48.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Drive/Drive.apk.* 2>/dev/null >> product/app/Drive/Drive.apk
+rm -f product/app/Drive/Drive.apk.* 2>/dev/null
+cat product/priv-app/PixelCameraServicesConnectivityClient/PixelCameraServicesConnectivityClient.apk.* 2>/dev/null >> product/priv-app/PixelCameraServicesConnectivityClient/PixelCameraServicesConnectivityClient.apk
+rm -f product/priv-app/PixelCameraServicesConnectivityClient/PixelCameraServicesConnectivityClient.apk.* 2>/dev/null
+cat product/priv-app/RecorderPrebuilt_725581069/RecorderPrebuilt_725581069.apk.* 2>/dev/null >> product/priv-app/RecorderPrebuilt_725581069/RecorderPrebuilt_725581069.apk
+rm -f product/priv-app/RecorderPrebuilt_725581069/RecorderPrebuilt_725581069.apk.* 2>/dev/null
+cat product/priv-app/PixelLiveWallpaperPrebuilt-26000013/PixelLiveWallpaperPrebuilt-26000013.apk.* 2>/dev/null >> product/priv-app/PixelLiveWallpaperPrebuilt-26000013/PixelLiveWallpaperPrebuilt-26000013.apk
+rm -f product/priv-app/PixelLiveWallpaperPrebuilt-26000013/PixelLiveWallpaperPrebuilt-26000013.apk.* 2>/dev/null
+cat product/priv-app/DevicePersonalizationPrebuiltPixel2025-playstore_aiai_20250306.00_RC10/DevicePersonalizationPrebuiltPixel2025-playstore_aiai_20250306.00_RC10.apk.* 2>/dev/null >> product/priv-app/DevicePersonalizationPrebuiltPixel2025-playstore_aiai_20250306.00_RC10/DevicePersonalizationPrebuiltPixel2025-playstore_aiai_20250306.00_RC10.apk
+rm -f product/priv-app/DevicePersonalizationPrebuiltPixel2025-playstore_aiai_20250306.00_RC10/DevicePersonalizationPrebuiltPixel2025-playstore_aiai_20250306.00_RC10.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/PrebuiltGmsCoreVic/PrebuiltGmsCoreVic.apk.* 2>/dev/null >> product/priv-app/PrebuiltGmsCoreVic/PrebuiltGmsCoreVic.apk
+rm -f product/priv-app/PrebuiltGmsCoreVic/PrebuiltGmsCoreVic.apk.* 2>/dev/null
+cat product/priv-app/GoogleDialer/GoogleDialer.apk.* 2>/dev/null >> product/priv-app/GoogleDialer/GoogleDialer.apk
+rm -f product/priv-app/GoogleDialer/GoogleDialer.apk.* 2>/dev/null
+cat product/priv-app/Phonesky/Phonesky.apk.* 2>/dev/null >> product/priv-app/Phonesky/Phonesky.apk
+rm -f product/priv-app/Phonesky/Phonesky.apk.* 2>/dev/null
+cat product/priv-app/PrebuiltBugle/PrebuiltBugle.apk.* 2>/dev/null >> product/priv-app/PrebuiltBugle/PrebuiltBugle.apk
+rm -f product/priv-app/PrebuiltBugle/PrebuiltBugle.apk.* 2>/dev/null
+cat product/priv-app/GoogleCamera/GoogleCamera.apk.* 2>/dev/null >> product/priv-app/GoogleCamera/GoogleCamera.apk
+rm -f product/priv-app/GoogleCamera/GoogleCamera.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat modem/images/g5400i-250908-251105-B-14386369/modem.bin.* 2>/dev/null >> modem/images/g5400i-250908-251105-B-14386369/modem.bin
+rm -f modem/images/g5400i-250908-251105-B-14386369/modem.bin.* 2>/dev/null
+cat vendor_boot.img.* 2>/dev/null >> vendor_boot.img
+rm -f vendor_boot.img.* 2>/dev/null
+cat system/system/apex/com.google.android.virt.apex.* 2>/dev/null >> system/system/apex/com.google.android.virt.apex
+rm -f system/system/apex/com.google.android.virt.apex.* 2>/dev/null
+cat vendor/apex/com.google.android.hardware.biometrics.face.apex.* 2>/dev/null >> vendor/apex/com.google.android.hardware.biometrics.face.apex
+rm -f vendor/apex/com.google.android.hardware.biometrics.face.apex.* 2>/dev/null
+cat vendor/apex/com.google.pixel.camera.hal.apex.* 2>/dev/null >> vendor/apex/com.google.pixel.camera.hal.apex
+rm -f vendor/apex/com.google.pixel.camera.hal.apex.* 2>/dev/null
+cat vendor/lib64/libspeechenhancer.so.* 2>/dev/null >> vendor/lib64/libspeechenhancer.so
+rm -f vendor/lib64/libspeechenhancer.so.* 2>/dev/null
+cat vendor/lib64/libedgetpu_tflite_compiler.so.* 2>/dev/null >> vendor/lib64/libedgetpu_tflite_compiler.so
+rm -f vendor/lib64/libedgetpu_tflite_compiler.so.* 2>/dev/null
+cat vendor/lib64/libufwriter.so.* 2>/dev/null >> vendor/lib64/libufwriter.so
+rm -f vendor/lib64/libufwriter.so.* 2>/dev/null
